13db194bea2f093eb701d41ee457a469848369e0
Adam Berecz <adam@laraform.io>
2021-07-27 08:31:26 +0200

    default hidden

--- src/themes/default/components/elements/HiddenElement.vue
+++ src/themes/default/components/elements/HiddenElement.vue
@@ -14,8 +14,4 @@
 </script>
 
 <style lang="scss">
-  @import 'node_modules/bootstrap/scss/_functions.scss';
-  @import 'node_modules/bootstrap/scss/_variables.scss';
-  @import 'node_modules/bootstrap/scss/_mixins.scss';
-
 </style>
