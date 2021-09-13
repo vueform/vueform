96bc4b14c62a77b9ce01a4d620b8261486e8a870
Adam Berecz <adam@laraform.io>
2021-09-13 15:18:43 +0200

    useOrder docs done

--- src/composables/elements/useOrder.js
+++ src/composables/elements/useOrder.js
@@ -12,10 +12,11 @@ const base = function(props, context, dependencies, options)
   // =============== METHODS ==============
 
   /**
-   * Helper method used to refresh the element's value which stores the order.
+   * Sets the value of `storeOrder` fields within a list of items to match the order.
    *
-   * @private
+   * @param {array} value* list of items
    * @returns {void}
+   * @private
    */
   const refreshOrderStore = (value) => {
     if (storeOrder.value) {
@@ -32,7 +33,7 @@ const base = function(props, context, dependencies, options)
   }
 
   /**
-   * 
+   * The name of the field which we should order by.
    * 
    * @type {string}
    */
