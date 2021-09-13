fb97d3bc337eca3c067e6d4740d72324e85eed25
Adam Berecz <adam@laraform.io>
2021-09-13 13:59:19 +0200

    useInput docs done

--- src/composables/elements/useInput.js
+++ src/composables/elements/useInput.js
@@ -5,7 +5,7 @@ const base = function(props, context, dependencies)
   // ================ DATA ================
 
   /**
-   * 
+   * The main input field of the element.
    * 
    * @type {HTMLElement} 
    */
