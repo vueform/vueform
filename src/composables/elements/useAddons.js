b98f7eef366048e46175b10736d2103db96bcc32
Adam Berecz <adam@laraform.io>
2021-09-13 09:44:23 +0200

    useAddons done

--- src/composables/elements/useAddons.js
+++ src/composables/elements/useAddons.js
@@ -1,23 +1,6 @@
-import { computed, toRefs } from 'composition-api'
-
 const base = function(props, context, dependencies)
 {
-  const {
-    addons
-  } = toRefs(props)
-
-  // ============== COMPUTED ==============
-  
-  /**
-   * Helper property used to determine internally if the element has any addons.
-   * 
-   * @type {boolean}
-   */
-  const hasAddon = computed(() => Object.keys(addons.value).length > 0)
-
-  return {
-    hasAddon,
-  }
+  // Technical composable to keep addon tests
 }
 
 export default base
