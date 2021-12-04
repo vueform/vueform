b36fb4525004c27b138af02e1d138123f66a1668
Adam Berecz <adam@laraform.io>
2021-12-04 12:17:16 +0100

    Added inline safelist for Tailwind JIT.

--- themes/tailwind/columns.js
+++ themes/tailwind/columns.js
@@ -1,4 +1,20 @@
 export default function (breakpoint, size) {
+  const safelist = [
+    'w-1/12',
+    'w-2/12',
+    'w-3/12',
+    'w-4/12',
+    'w-5/12',
+    'w-6/12',
+    'w-7/12',
+    'w-8/12',
+    'w-9/12',
+    'w-10/12',
+    'w-11/12',
+    'w-12/12',
+    'w-full',
+  ]
+  
   switch (breakpoint) {
     case 'default':
       return size == 12 ? 'w-full' : `w-${size}/12`
