e26ca26d1ac07c20a0d62e2304a9b7edd8e96b50
Adam Berecz <adam@laraform.io>
2021-12-04 18:11:35 +0100

    import lodash in baseElement

--- src/composables/elements/useBaseElement.js
+++ src/composables/elements/useBaseElement.js
@@ -1,8 +1,8 @@
+import _ from 'lodash'
 import {
   computed, getCurrentInstance, provide, toRefs, onBeforeMount, onMounted, onBeforeUpdate,
   onUpdated, onBeforeUnmount, onUnmounted, ref,
 } from 'composition-api'
-
 import useParentAssign from './../useParentAssign'
 
 const base = function(props, context, dependencies)
