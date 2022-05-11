3a24734bee36f1cd2790ede1826b971406b92204
Adam Berecz <adam@laraform.io>
2022-05-11 08:13:20 +0200

    feat: input @blur

---
+++ src/composables/elements/useHandleBlur.js
@@ -0,0 +1,25 @@
+const base = function(props, context, dependencies)
+{
+  // ============ DEPENDENCIES ============
+
+  const fire = dependencies.fire
+  const el$ = dependencies.el$
+
+  // =============== METHODS ==============
+
+  /**
+   * Handles `blur` event.
+   *
+   * @returns {void}
+   * @private
+   */
+  const handleBlur = () => {
+    fire('blur', el$.value)
+  }
+
+  return {
+    handleBlur,
+  }
+}
+
+export default base
