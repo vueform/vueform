d4b73226303f4e5bd65288e48c11196796f25e01
Adam Berecz <adam@laraform.io>
2022-05-11 08:14:34 +0200

    fix: dates value calc fix

--- src/composables/elements/useValue.js
+++ src/composables/elements/useValue.js
@@ -45,8 +45,8 @@ const base = function(props, context, dependencies, options = {})
    * 
    * @type {any}
    */
-  const value = computed(options.value || {
-    get() {
+  const value = computed({
+    get: options.value?.get || function() {
       let value
 
       if (form$.value.isSync) {
@@ -59,7 +59,7 @@ const base = function(props, context, dependencies, options = {})
 
       return value !== undefined ? value : (defaultValue.value instanceof File ? defaultValue.value : _.cloneDeep(defaultValue.value))
     },
-    set(val) {
+    set: options.value?.set || function(val) {
       if (form$.value.isSync) {
         form$.value.updateModel(dataPath.value, val)
       } else if (parent.value && ['list', 'multifile'].indexOf(parent.value.type) !== -1) {
@@ -207,18 +207,43 @@ const multilingual = function(props, context, dependencies)
 
 const date = function(props, context, dependencies)
 {
+  const { name } = toRefs(props)
+
   // ============ DEPENDENCIES =============
 
+  const parent = dependencies.parent
   const valueDateFormat = dependencies.valueDateFormat
+  const defaultValue = dependencies.defaultValue
   const dataPath = dependencies.dataPath
   const form$ = dependencies.form$
 
+  // ================= PRE =================
+
+  /**
+   * The store for the value of the element when we're not using external data (form's `v-model`).
+   * 
+   * @type {any}
+   * @private
+   */
+  const internalValue = ref(defaultValue.value instanceof File ? defaultValue.value : _.cloneDeep(defaultValue.value))
+
   const {
     value,
+    initialValue,
   } = base(props, context, dependencies, {
     value: {
       get() {
-        return _.get(form$.value.model, dataPath.value)
+        let value
+
+        if (form$.value.isSync) {
+          value = _.get(form$.value.model, dataPath.value)
+        } else if (parent.value && ['object', 'list', 'multifile'].indexOf(parent.value.type) !== -1) {
+          value = parent.value.value[name.value]
+        } else {
+          value = internalValue.value
+        }
+
+        return value !== undefined ? value : (defaultValue.value instanceof File ? defaultValue.value : _.cloneDeep(defaultValue.value))
       },
       set(val) {
         // If the value is not a Date object check if it is matching the value format
@@ -226,10 +251,22 @@ const date = function(props, context, dependencies)
           checkDateFormat(valueDateFormat.value, val)
         }
 
-        form$.value.updateModel(dataPath.value, val && val instanceof Date && valueDateFormat.value !== false
+        val = val && val instanceof Date && valueDateFormat.value !== false
           ? moment(val).format(valueDateFormat.value)
           : val
-        )
+
+        if (form$.value.isSync) {
+          form$.value.updateModel(dataPath.value, val)
+        } else if (parent.value && ['list', 'multifile'].indexOf(parent.value.type) !== -1) {
+          const newValue = parent.value.value.map((v,k) => k == name.value ? val : v)
+          parent.value.update(newValue)
+        } else if (parent.value && ['object'].indexOf(parent.value.type) !== -1) {
+          parent.value.value = Object.assign({}, parent.value.value, {
+            [name.value]: val
+          })
+        } else {
+          internalValue.value = val
+        }
       }
     }
   })
@@ -243,30 +280,57 @@ const date = function(props, context, dependencies)
   return {
     value,
     model,
+    initialValue,
+    internalValue,
   }
 }
 
 const dates = function(props, context, dependencies)
 {
+  const { name } = toRefs(props)
+  
   // ============ DEPENDENCIES =============
 
+  const parent = dependencies.parent
   const valueDateFormat = dependencies.valueDateFormat
+  const defaultValue = dependencies.defaultValue
   const dataPath = dependencies.dataPath
   const form$ = dependencies.form$
 
+  // ================= PRE =================
+
+  /**
+   * The store for the value of the element when we're not using external data (form's `v-model`).
+   * 
+   * @type {any}
+   * @private
+   */
+  const internalValue = ref(defaultValue.value instanceof File ? defaultValue.value : _.cloneDeep(defaultValue.value))
+
   const {
     value,
+    initialValue,
   } = base(props, context, dependencies, {
     value: {
       get() {
-        return _.get(form$.value.model, dataPath.value)
+        let value
+
+        if (form$.value.isSync) {
+          value = _.get(form$.value.model, dataPath.value)
+        } else if (parent.value && ['object', 'list', 'multifile'].indexOf(parent.value.type) !== -1) {
+          value = parent.value.value[name.value]
+        } else {
+          value = internalValue.value
+        }
+
+        return value !== undefined ? value : (defaultValue.value instanceof File ? defaultValue.value : _.cloneDeep(defaultValue.value))
       },
       set(val) {
         if (!Array.isArray(val)) {
           val = [val]
         }
 
-        form$.value.updateModel(dataPath.value, val.map((v) => {
+        val = val.map((v) => {
           if (!_.isEmpty(v) && !(v instanceof Date) && valueDateFormat.value !== false) {
             checkDateFormat(valueDateFormat.value, v)
           }
@@ -274,7 +338,20 @@ const dates = function(props, context, dependencies)
           return v && v instanceof Date && valueDateFormat.value !== false
             ? moment(v).format(valueDateFormat.value)
             : v
-        }))
+        })
+
+        if (form$.value.isSync) {
+          form$.value.updateModel(dataPath.value, val)
+        } else if (parent.value && ['list', 'multifile'].indexOf(parent.value.type) !== -1) {
+          const newValue = parent.value.value.map((v,k) => k == name.value ? val : v)
+          parent.value.update(newValue)
+        } else if (parent.value && ['object'].indexOf(parent.value.type) !== -1) {
+          parent.value.value = Object.assign({}, parent.value.value, {
+            [name.value]: val
+          })
+        } else {
+          internalValue.value = val
+        }
       }
     }
   })
@@ -290,6 +367,8 @@ const dates = function(props, context, dependencies)
   return {
     value,
     model,
+    initialValue,
+    internalValue,
   }
 }
 
