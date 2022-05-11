913714b494c75c7970bd4ce8dc4713b5ea0ba696
Adam Berecz <adam@laraform.io>
2022-05-11 08:19:41 +0200

    fix: el label wrapper class

--- themes/blank/templates/ElementLabel.vue
+++ themes/blank/templates/ElementLabel.vue
@@ -1,19 +1,19 @@
 <template>
   <!-- If label is a component -->
   <label v-if="label && isLabelComponent" :class="classes.container" :for="name">
-    <span><component v-if="isLabelComponent" :is="label"/></span>
+    <span :class="classes.wrapper"><component v-if="isLabelComponent" :is="label"/></span>
     <ElementInfo><slot name="info"/></ElementInfo>
   </label>
 
   <!-- If label is HTML -->
   <label v-else-if="label" :class="classes.container" :for="name">
-    <span v-html="label"></span>
+    <span :class="classes.wrapper" v-html="label"></span>
     <ElementInfo><slot name="info"/></ElementInfo>
   </label>
 
   <!-- If label is a slot -->
   <label v-else-if="isSlot" :class="classes.container" :for="name">
-    <slot/>
+    <span :class="classes.wrapper"><slot/></span>
     <ElementInfo><slot name="info"/></ElementInfo>
   </label>
 
@@ -29,6 +29,7 @@
         merge: true,
         defaultClasses: {
           container: '',
+          wrapper: '',
         }
       }
     }
