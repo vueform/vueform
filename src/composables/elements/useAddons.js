81dae6f31e56c4a7066486ea3a958e7ed1171a1f
Adam Berecz <adam@laraform.io>
2022-05-11 08:01:24 +0200

    fix: Recognize addons from schema slots

--- src/composables/elements/useAddons.js
+++ src/composables/elements/useAddons.js
@@ -2,7 +2,7 @@ import { toRefs, computed } from 'composition-api'
 
 const base = function(props, context, dependencies)
 {
-  const { addons } = toRefs(props)
+  const { addons, slots } = toRefs(props)
 
   // ============ DEPENDENCIES ============
 
@@ -11,11 +11,11 @@ const base = function(props, context, dependencies)
   // ============== COMPUTED ==============
 
   const hasAddonBefore = computed(() => {
-    return !!(addons.value.before || el$.value.$slots?.['addon-before'] || (context.expose === undefined && el$.value.$scopedSlots?.['addon-before']))
+    return !!(addons.value.before || el$.value.$slots?.['addon-before'] || (context.expose === undefined && el$.value.$scopedSlots?.['addon-before']) || slots.value['addon-before'])
   })
 
   const hasAddonAfter = computed(() => {
-    return !!(addons.value.after || el$.value.$slots?.['addon-after'] || (context.expose === undefined && el$.value.$scopedSlots?.['addon-after']))
+    return !!(addons.value.after || el$.value.$slots?.['addon-after'] || (context.expose === undefined && el$.value.$scopedSlots?.['addon-after']) || slots.value['addon-after'])
   })
 
   return {
