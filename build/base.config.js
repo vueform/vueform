5102d24b468c229af3e577aa3f691234ffe113c0
Adam Berecz <adam@laraform.io>
2022-07-14 19:27:40 +0200

    feat: build fix

--- build/base.config.js
+++ build/base.config.js
@@ -1,6 +1,6 @@
 import ncp from 'ncp'
 import babel from '@rollup/plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
+import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import path from 'path'
