ed2d1f6284936e615806387f0ab565bc05defd7d
Adam Berecz <adam@laraform.io>
2023-02-03 14:46:47 +0100

    feat: conditions, all accepted when array

--- src/utils/compare.js
+++ src/utils/compare.js
@@ -13,22 +13,22 @@ export default function(actual, operator, expected, el$) {
   switch (operator.toLowerCase()) {
     case '>':
       return _.isArray(actual)
-        ? _.isArray(expected) ? actual.every(a => a > expected[0]) : actual.length > expected
+        ? actual.length && actual.every(a => a > expected)
         : actual > expected
 
     case '>=':
       return _.isArray(actual)
-        ? _.isArray(expected) ? actual.every(a => a >= expected[0]) : actual.length >= expected
+        ? actual.length && actual.every(a => a >= expected)
         : actual >= expected
 
     case '<':
       return _.isArray(actual)
-        ? _.isArray(expected) ? actual.every(a => a < expected[0]) : actual.length < expected
+        ? actual.length && actual.every(a => a < expected)
         : actual < expected
 
     case '<=':
       return _.isArray(actual)
-        ? _.isArray(expected) ? actual.every(a => a <= expected[0]) : actual.length <= expected
+        ? actual.length && actual.every(a => a <= expected)
         : actual <= expected
 
     case '==':
