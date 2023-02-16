a6422fa45f3444bdea16edc99beed47fb86fcaa0
Adam Berecz <adam@laraform.io>
2023-02-16 12:41:29 +0100

    chore: to docs

---
+++ build/docs.config.js
@@ -0,0 +1,47 @@
+import path from 'path'
+import obfuscator from 'rollup-plugin-obfuscator'
+
+export default (commandLineArgs) => {
+  const files = [
+    {
+      input: path.resolve(__dirname, '../dist/installer.js'),
+      output: path.resolve(__dirname, '../../vueform-docs/packages/vueform/installer.js'),
+      lock: true,
+    },
+    {
+      input: path.resolve(__dirname, '../dist/element.js'),
+      output: path.resolve(__dirname, '../../vueform-docs/packages/vueform/element.js'),
+      lock: true,
+    },
+  ]
+
+  return files.map((file) => {
+    let globalOptions = {
+      identifierNamesGenerator: 'mangled-shuffled',
+    }
+
+    if (file.lock) {
+      globalOptions = {
+        ...globalOptions,
+        domainLock: ['localhost', 'vueform.com', 'www.vueform.com', 'new.vueform.com', 'vueform.loc', 'builder.vueform.com', 'builder.vueform.loc'],
+        domainLockRedirectUrl: 'https://vueform.com/not-allowed?k=vueform'
+      }
+    }
+
+    return {
+      input: file.input,
+      output: {
+        file: file.output,
+        format: 'esm',
+        sourcemap: false,
+      },
+      plugins: [
+        obfuscator({
+          fileOptions: false,
+          globalOptions,
+        }),
+      ],
+      external: ['vue', 'axios', 'lodash', 'moment'],
+    }
+  })
+}
