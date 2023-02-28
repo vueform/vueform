5cc99981d1eb7db005710d53d54374bb0d009740
Adam Berecz <adam@laraform.io>
2023-02-28 09:17:23 +0100

    feat: responsive horizontal label tw 2 compat

--- tailwind.js
+++ tailwind.js
@@ -431,6 +431,7 @@ const vueform = plugin((context) => {
   let withFloating = {}
   let fullWidth = {}
   let textType = {}
+  let responsive = {}
 
   sizes.forEach((s) => {
     let suffix = s?'-'+s:''
@@ -672,11 +673,19 @@ const vueform = plugin((context) => {
       paddingTop: '0px'
     }
 
+    responsive['.form-pt-0'] = {
+      paddingTop: '0px'
+    }
+
     plain['.form-text-type .form-pt-0'] = {
       paddingTop: '0px'
     }
 
     textType[`.form-pt-input-border${suffix}`] = {
+      paddingTop: `calc(var(--vf-py-input${size}) + var(--vf-border-width-input-t))`,
+    }
+
+    responsive[`.form-pt-input-border${suffix}`] = {
       paddingTop: `calc(var(--vf-py-input${size}) + var(--vf-border-width-input-t))`,
     }
 
@@ -916,11 +925,23 @@ const vueform = plugin((context) => {
       paddingBottom: `calc(var(--vf-gutter${size}) / 3)`
     }
 
+    responsive[`.${e(`form-pb-gutter/3${suffix}`)}`] = {
+      paddingBottom: `calc(var(--vf-gutter${size}) / 3)`
+    }
+
     plain[`.form-pr-gutter${suffix}`] = {
       paddingRight: `var(--vf-gutter${size})`,
     }
 
+    responsive[`.form-pr-gutter${suffix}`] = {
+      paddingRight: `var(--vf-gutter${size})`,
+    }
+
     plain['.form-pr-0'] = {
+      paddingRight: '0px'
+    }
+
+    responsive['.form-pr-0'] = {
       paddingRight: '0px'
     }
 
@@ -1848,6 +1869,7 @@ const vueform = plugin((context) => {
   addUtilities(textType, ['text-type'])
   addUtilities(rtl, ['rtl'])
   addUtilities(infoGroupHoverable, ['info-group-hover'])
+  addUtilities(responsive, ['responsive'])
 
   addVariant('h', ({ modifySelectors, separator }) => {
     modifySelectors(({ className }) => {
