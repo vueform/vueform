30f7cdc96eeeb3195a729e886c3d05919cc99500
Adam Berecz <adam@laraform.io>
2023-03-06 10:07:50 +0100

    feat: localize async items

--- src/utils/localize.js
+++ src/utils/localize.js
@@ -1,5 +1,5 @@
 export default function localize (object, $config) {
   return object && typeof object === 'object'
-    ? object?.[$config.i18n.locale] || object?.[$config.i18n.fallbackLocale] || object?.[Object.keys(object)[0]]
+    ? object?.[$config.i18n.locale] || object?.[$config.i18n.locale.toUpperCase()] || object?.[$config.i18n.fallbackLocale] || object?.[$config.i18n.fallbackLocale.toUpperCase()] || object?.[Object.keys(object)[0]]
     : object
 }
