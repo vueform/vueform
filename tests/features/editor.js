0c9cc1484cb90b2e7c61ade05e9017a41353cb6d
szabolcsmucsi <szabolcsmucsi@gmail.com>
2023-05-18 11:08:07 +0200

    test: editor revisioned

--- tests/features/editor.js
+++ tests/features/editor.js
@@ -1,4 +1,4 @@
-import { createForm, findAllComponents, testPropDefault, testAttribute, destroy } from 'test-helpers'
+import { createForm, findAllComponents, destroy } from 'test-helpers'
 
 export const accept = function (elementType, elementName, options) {
   it('should pass `accept` to Editor component', () => {
@@ -21,6 +21,7 @@ export const accept = function (elementType, elementName, options) {
   })
 }
 
+
 export const acceptMimes = function (elementType, elementName, options) {
   it('should pass `acceptMimes` to Editor component', () => {
     let form = createForm({
@@ -42,8 +43,16 @@ export const acceptMimes = function (elementType, elementName, options) {
   })
 }
 
+
+export const focused = function (elementType, elementName, options) {
+
+}
+
+
 export const editorEndpoint = function (elementType, elementName, options) {
+
   it('should equal to config value by default', () => {
+
     let form = createForm({
       schema: {
         el: {
@@ -56,8 +65,9 @@ export const editorEndpoint = function (elementType, elementName, options) {
 
     expect(el.editorEndpoint).toStrictEqual(form.vm.$vueform.config.endpoints.attachment.url)
   })
-  
+
   it('should pass `endpoint` to Editor component', () => {
+
     let form = createForm({
       schema: {
         el: {
@@ -71,6 +81,44 @@ export const editorEndpoint = function (elementType, elementName, options) {
     let Editor = findAllComponents(elWrapper, { name: 'EditorWrapper' }).at(0)
 
     expect(Editor.props('endpoint')).toStrictEqual(el.editorEndpoint)
+
+    // destroy() // teardown
+  })
+}
+
+export const editorMethod = function (elementType, elementName, options) {
+
+  it('should equal to config value by default', () => {
+
+    let form = createForm({
+      schema: {
+        el: {
+          type: elementType,
+        }
+      }
+    })
+
+    let el = form.vm.el$('el')
+
+    expect(el.editorMethod).toStrictEqual(form.vm.$vueform.config.endpoints.attachment.method)
+  })
+
+
+  it('should pass `method` to Editor component', () => {
+
+    let form = createForm({
+      schema: {
+        el: {
+          type: elementType,
+        }
+      }
+    })
+
+    let el = form.vm.el$('el')
+    let elWrapper = findAllComponents(form, { name: elementName }).at(0)
+    let Editor = findAllComponents(elWrapper, { name: 'EditorWrapper' }).at(0)
+
+    expect(Editor.props('method')).toStrictEqual(el.editorMethod)
 
     // destroy() // teardown
   })
