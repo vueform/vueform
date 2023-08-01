5b147763ee0175ca473f3753435a13cd1ebe6876
Adam Berecz <adam@laraform.io>
2023-08-01 15:27:48 +0200

    chore: release script

--- sandbox.sh
+++ sandbox.sh
@@ -9,9 +9,20 @@ error_message() {
 
 new_version=1.3.5
 
-# Undo any unpushed commits
-unpushed_commits=$(git log origin/next..HEAD --oneline)
-if [ -n "$unpushed_commits" ]; then
-    git reset origin/main --hard
+# Check if there are any uncommitted changes
+git_status=$(git status --porcelain)
+if [ -n "$git_status" ]; then
+    # Save the changes in the working directory to a temporary stash
+    git stash save --include-untracked --quiet
+    stash_result=$?
+    if [ $stash_result -ne 0 ]; then
+        # Echo the message in red color
+        error_message "Git stash failed in $repo. Exiting..."
+        exit 1
+    fi
+
+    # Revert the changes from the temporary stash (to effectively undo unpushed commits)
+    git stash drop --quiet
+
     success_message "Unpushed commits undone in $repo."
 fi
