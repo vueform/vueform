36cd96b38412f05caa9297ea704c6cb104737228
Adam Berecz <adam@laraform.io>
2023-08-01 08:24:10 +0200

    chore: release script

---
+++ build/themes.config.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+
+import postcss from 'rollup-plugin-postcss'
+import autoprefixer from 'autoprefixer'
+
+const files = [
+  {
+    minimize: false,
+    input: 'themes/vueform/scss/index.scss',
+    output: 'themes/vueform/css/index.css',
+  },
+  {
+    minimize: true,
+    input: 'themes/vueform/scss/index.scss',
+    output: 'themes/vueform/css/index.min.css',
+  },
+  {
+    minimize: false,
+    input: 'themes/material/scss/index.scss',
+    output: 'themes/material/css/index.css',
+  },
+  {
+    minimize: true,
+    input: 'themes/material/scss/index.scss',
+    output: 'themes/material/css/index.min.css',
+  },
+  {
+    minimize: false,
+    input: 'themes/tailwind-material/scss/index.scss',
+    output: 'themes/tailwind-material/css/index.css',
+  },
+  {
+    minimize: true,
+    input: 'themes/tailwind-material/scss/index.scss',
+    output: 'themes/tailwind-material/css/index.min.css',
+  },
+  {
+    minimize: true,
+    input: 'themes/bootstrap/css/index.css',
+    output: 'themes/bootstrap/css/index.min.css',
+  },
+]
+
+export default files.map((file) => {
+  return {
+    input: file.input,
+    output: {
+      file: file.output,
+      format: 'esm',
+    },
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: file.minimize,
+        plugins: [
+          autoprefixer(),
+        ]
+      }),
+    ]
+  }
+})
