de7f952b5c13efdc49beddaa51398d973de02c75
Adam Berecz <adam@laraform.io>
2023-08-02 08:33:23 +0200

    chore: version, build 1.3.9

--- sandbox.sh
+++ sandbox.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 success_message() {
     echo -e "\033[32m>>> $1\033[0m"
 }
@@ -7,22 +9,37 @@ error_message() {
     echo -e "\033[31m>>> $1\033[0m"
 }
 
-new_version=1.3.5
+# Function to display error message in red color
+info_message() {
+    echo ">>> $1"
+}
 
-# Check if there are any uncommitted changes
-git_status=$(git status --porcelain)
-if [ -n "$git_status" ]; then
-    # Save the changes in the working directory to a temporary stash
-    git stash save --include-untracked --quiet
-    stash_result=$?
-    if [ $stash_result -ne 0 ]; then
-        # Echo the message in red color
-        error_message "Git stash failed in $repo. Exiting..."
-        exit 1
-    fi
+info_message "Generating release notes..."
+node ./scripts/notes.js
 
-    # Revert the changes from the temporary stash (to effectively undo unpushed commits)
-    git stash drop --quiet
+info_message "Copying release notes to vueform.com/apps/sdk/release.json..."
+cp ./CHANGELOG.json ./../vueform.com/apps/sdk/releases.json
 
-    success_message "Unpushed commits undone in $repo."
+info_message "Comitting release notes..."
+    
+cd ./../vueform.com
+
+git add apps/sdk/releases.json
+git commit -m  '$new_version'
+git push
+
+info_message "Publishing release notes..."
+
+# Define the URL
+url="https://forge.laravel.com/servers/681079/sites/2056602/deploy/http?token=gTbJpYLM7MhxprrI4m5WkceH8vA16AHkW2P4Q4ig"
+
+# Make the GET request using curl
+response=$(curl -sS $url)
+
+# Check if the request was successful (HTTP status code 200)
+if [[ $? -eq 0 ]]; then
+  success_message "https://sdk.vueform.com deploy script ran successfully! You should see the release notes in a few minutes."
+else
+  error_message "https://sdk.vueform.com deploy script failed. Exiting..."
+  exit 1
 fi
