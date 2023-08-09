fcbc3f0042a0f59d7d5477e4d3cbbf2f238cfac5
Adam Berecz <adam@laraform.io>
2023-08-09 07:46:45 +0200

    feat: vite plugin

---
+++ vite-plugin-vueform.js
@@ -0,0 +1,14 @@
+module.exports = function vitePluginVueform() {
+  return {
+    name: 'vueform',
+    config: () => ({
+      optimizeDeps: {
+        include: [
+          'wnumb',
+          'nouislider',
+          'trix'
+        ]
+      }
+    })
+  }
+}
