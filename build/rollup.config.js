ffd14970bac090926605d5e0328d9dc3198f09ad
Adam Berecz <adam@laraform.io>
2023-10-25 07:57:42 +0200

    chore: license banner

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -3,6 +3,7 @@ import json from '@rollup/plugin-json'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
+import license from 'rollup-plugin-license'
 import { terser } from 'rollup-plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
@@ -11,6 +12,7 @@ const files = [
     type: 'script',
     input: 'src/index.js',
     output: 'dist/index.js',
+    banner: true,
   },
   {
     type: 'style',
@@ -56,29 +58,50 @@ const files = [
   },
 ]
 
-export default files.map((file) => ({
-  input: file.input,
-  output: {
-    file: file.output,
-    format: 'esm',
-  },
-  plugins: file.type === 'script' ? [
-    json(),
-    commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: /^(.+\/)?node_modules\/.+$/,
-    }),
-    nodeResolve(),
-    terser(),
-  ] : (file.type === 'style' ? [
-    postcss({
-      extract: true,
-      minimize: file.minimize,
-      plugins: [
-        autoprefixer(),
-      ]
-    }),
-  ] : []),
-  external: ['vue', 'axios', 'lodash', 'moment']
-}))
+export default files.map((file) => {
+  let plugins = []
+
+  if (file.type === 'script') {
+    plugins = [
+      json(),
+      commonjs(),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: /^(.+\/)?node_modules\/.+$/,
+      }),
+      nodeResolve(),
+      terser(),
+    ]
+
+    if (file.banner) {
+      plugins.push(license({
+        banner: {
+          content: `Vueform v<%= pkg.version %> (https://github.com/vueform/vueform)\n` + 
+                   `Copyright (c) <%= moment().format('YYYY') %> Adam Berecz <adam@vueform.com>\n` + 
+                   `Licensed under the MIT License`,
+          commentStyle: 'ignored',
+        }
+      }))
+    }
+  } else if (file.type === 'style') {
+    plugins = [
+      postcss({
+        extract: true,
+        minimize: file.minimize,
+        plugins: [
+          autoprefixer(),
+        ]
+      }),
+    ]
+  }
+
+  return {
+    input: file.input,
+    output: {
+      file: file.output,
+      format: 'esm',
+    },
+    plugins,
+    external: ['vue', 'axios', 'lodash', 'moment']
+  }
+})
