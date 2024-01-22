fb82cc17b5dd59c06d623d6e3d6f38d59eb35c2b
Adam Berecz <adam@laraform.io>
2024-01-22 11:19:18 +0100

    chore: tw prefixer

--- tailwind-prefixer.js
+++ tailwind-prefixer.js
@@ -1,23 +1,32 @@
 module.exports = function (content, prefix = '', sizes = ['sm', 'md', 'lg', 'xl', '2xl']) {
-  if (content.match('^// prefix')) {
-    content = content.replace(/(['`])(.*)\1/g, function(match) {
-      return `${match.match(/(['`])/)[1]}${match.match(/(['`])(.*)\1/)[2].split(' ').map((c) => {
-        if (c.match(/:/)) {
-          return c.replace(':', `:${prefix}`)
-        }
-        if (c.match(/!/)) {
-          return c.replace('!', `!${prefix}`)
-        }
-        
-        return (c.length ? `${prefix}${c}` : c)
-      }).join(' ')}${match.match(/(['`])/)[1]}`
+  if (content.match('form-bg-input')) {
+    // Regular expression to match strings in single quotes, double quotes, or backticks
+    const regex = /(['"`])(.*?)\1/g;
+
+    // Replace the found strings with the prefixed version
+    content = content.replace(regex, (match, quote, content) => {
+        // Prefix each word if the content has spaces
+        content = content.split(' ').map(word => {
+          if (word.match(/:/)) {
+            return word.replace(':', `:${prefix}`)
+          } else if (word.match(/!/)) {
+            return word.replace('!', `!${prefix}`)
+          } else {
+            return prefix + word
+          }
+        }).join(' ');
+        return `${quote}${content}${quote}`;
+    });
+
+    const widths = [
+      'col-span-1', 'col-span-2', 'col-span-3', 'col-span-4', 'col-span-5', 'col-span-6', 'col-span-7', 'col-span-8', 'col-span-9', 'col-span-10', 'col-span-11', 'col-span-12',
+    ]
+
+    widths.forEach((width) => {
+      content += `\n${prefix}${width}`
     })
 
     Object.values(sizes).forEach((size) => {
-      const widths = [
-        'col-span-1', 'col-span-2', 'col-span-3', 'col-span-4', 'col-span-5', 'col-span-6', 'col-span-7', 'col-span-8', 'col-span-9', 'col-span-10', 'col-span-11', 'col-span-12',
-      ]
-
       widths.forEach((width) => {
         content += `\n${size}:${prefix}${width}`
       })
