af1a838f11a03f38f9bb14e30f11903b18bca9c7
Adam Berecz <adam@laraform.io>
2024-01-24 10:57:30 +0100

    fix: in rule fix #121

--- src/services/validation/rules/in.js
+++ src/services/validation/rules/in.js
@@ -4,7 +4,6 @@ import normalize from './../../../utils/normalize'
 
 export default class in_ extends Validator {
   check(value) {
-    console.log(values(this.attributes))
     return values(this.attributes).indexOf(normalize(String(value).trim())) !== -1
   }
 }
