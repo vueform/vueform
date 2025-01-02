4e2973a0fd6fdc1077311c9852e52eb7914584f7
Adam Berecz <adam@laraform.io>
2025-01-02 11:43:53 +0100

    feat: allow translatable static content

--- src/composables/elements/useStatic.js
+++ src/composables/elements/useStatic.js
@@ -1,4 +1,5 @@
-import { computed, toRefs } from 'vue'
+import { computed, toRefs, inject } from 'vue'
+import localize from './../../utils/localize'
 
 const base = function(props, context, dependencies)
 {
@@ -10,8 +11,13 @@ const base = function(props, context, dependencies)
   
   const {
     fieldSlots,
-    el$
+    el$,
+    form$,
   } = dependencies
+  
+  // =============== INJECT ===============
+  
+  const config$ = inject('config$')
   
   // ============== COMPUTED ==============
   
@@ -31,7 +37,19 @@ const base = function(props, context, dependencies)
    * @type {any}
    */
   const resolvedContent = computed(() => {
-    return typeof content.value === 'function' ? content.value(el$.value) : content.value
+    let resolvedContent = typeof content.value === 'function' ? content.value(el$.value) : content.value
+
+    // Content is multilingual
+    if (content.value && typeof content.value === 'object' && !content.value?.render && !content.value?.template) {
+      resolvedContent = Object.keys(resolvedContent).reduce((prev, curr) => ({
+        ...prev,
+        [curr]: typeof resolvedContent[curr] === 'function' ? resolvedContent[curr](el$.value) : resolvedContent[curr]
+      }), {})
+
+      resolvedContent = localize(resolvedContent, config$.value, form$.value)
+    }
+
+    return resolvedContent
   })
 
   /**
@@ -41,11 +59,11 @@ const base = function(props, context, dependencies)
    * @private
    */
   const componentContent = computed(() => {
-    if (!content.value?.render && !content.value?.template) {
-      return content.value
+    if (!resolvedContent.value?.render && !resolvedContent.value?.template) {
+      return resolvedContent.value
     }
 
-    return resolveComponent(content.value)
+    return resolveComponent(resolvedContent.value)
   })
 
   /**
