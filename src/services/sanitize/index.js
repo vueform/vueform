78808e8d7c7735c89af3dfdfc01f861e3a382226
Adam Berecz <adam@laraform.io>
2025-04-03 14:50:20 +0200

    feat: sanitize v-html

--- src/services/sanitize/index.js
+++ src/services/sanitize/index.js
@@ -1,13 +1,13 @@
 import DOMPurify from 'dompurify'
 
 export default function(options, init, enabled) {
-  return (str) => {
-    if (!enabled) {
-      return str
+  return (input) => {
+    if (!enabled || typeof input !== 'string') {
+      return input
     }
 
     const purify = typeof init === 'function' ? init(DOMPurify) : DOMPurify
 
-    return purify.sanitize(str, options)
+    return purify.sanitize(input, options)
   }
 }
