8579f6dbcf89aed61e6955b9937afc6c538bd723
Adam Berecz <adam@laraform.io>
2025-06-04 11:50:34 +0200

    chore: version, cl, build, nuxt

--- dist/tailwind-material.mjs
+++ dist/tailwind-material.mjs
@@ -1,5 +1,5 @@
 /*!
- * Vueform v1.12.8 (https://github.com/vueform/vueform)
+ * Vueform v1.12.9 (https://github.com/vueform/vueform)
  * Copyright (c) 2025 Adam Berecz <adam@vueform.com>
  * Licensed under the MIT License
  */
