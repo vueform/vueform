ac4675ed3c8ac9c7facc5f695fe034efac31c172
Adam Berecz <adam@laraform.io>
2025-07-17 13:15:43 +0200

    fix: `gt`, `gte`, `lt`, `lte` rules that point to another elemegit pount should take into account numeric types #466

--- src/services/validation/rules/gte.js
+++ src/services/validation/rules/gte.js
@@ -2,7 +2,7 @@ import gt from './gt'
 
 export default class gte extends gt {
   compare(value, otherValue) {
-    let otherSize = this.size(otherValue)
+    let otherSize = this.size(otherValue, this.other$)
 
     return otherSize == 0 || this.size(value) >= otherSize
   }
